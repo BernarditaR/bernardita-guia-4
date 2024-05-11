78e03f10a4ab09f9f7c08025a8933c9ad7cf2b49
[BernarditaR] <[bernardita.rivera@ig.edu.ar]>
2024-05-11 16:21:38 -0300

    Cuarto ejercicio

--- eje4.sh
+++ eje4.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#numeros o variables a sumar
 numero1=20
+
 numero2=5
 
-#suma de los valores definidos anteriormente
+
 resultado=$((numero1 + numero2))
 
+echo "La suma de $numero1 y $numero2 es: $resultado"
