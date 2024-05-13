b598492cf0bc33aa866b556a6f6318ffd340b212
BernarditaR <bernardita.rivera@ig.edu.ar>
2024-05-13 13:56:03 -0300

    Noceno ejercicio

--- eje11.sh
+++ eje11.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+
+
+
+while true; do
+    hora_actual=$(date +"%H:%M:%S")
+    bytes=$(cat /sys/class/net/enp2s0/statistics/rx_bytes)
+    echo 'Hora' $hora_actual 'cantidad de bytes:' $bytes 
+    sleep 1
+done
+
